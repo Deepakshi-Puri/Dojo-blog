216e6e38f46c08a7e8f9e4aeadcc8be3f5dd5b6d
Deepakshi <purideepakshi24@gmail.com>
2023-01-31 16:10:21 +0530

    Modified again

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import Navbar from './Navbar';
-import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar/>
+        <Navbar />
         <div className="content">
           <Switch>
             <Route exact path="/">
-              <Home/>
+              <Home />
+            </Route>
+            <Route exact path="/create">
+              <Create/>
+            </Route>
+            <Route exact path="/blogs/:id">
+              <BlogDetails/>
+            </Route>
+            <Route path= "*">
+              <NotFound/>
             </Route>
           </Switch>
         </div>
