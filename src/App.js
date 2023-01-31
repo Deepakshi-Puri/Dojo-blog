544daff34b0d739a0f78662fb62b4da697e2da66
Deepakshi <purideepakshi24@gmail.com>
2023-01-31 16:09:38 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <div className="content">
-        <h1>App Component</h1>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home/>
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
